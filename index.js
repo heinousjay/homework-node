dd09721fab6c2e9f74c2d4ec5a122d012d62758f
Ben Drucker <bvdrucker@gmail.com>
2016-07-09 00:25:41 -0700

    Clear out index

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 'use strict'
 
-const pump = require('pump')
-const depended = require('./depended')
-const latest = require('./latest')
-const download = require('./download')
-
 module.exports = downloadPackages
 
 function downloadPackages (count, callback) {
-  pump(depended(count), latest(), download('./packages'), callback)
+
 }
